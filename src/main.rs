0e324a548cfe021c2d59a751556d8561d14d652c
Brian Wilens <brian.wilens@gmail.com>
2023-05-28 22:03:21 -0500

    ade guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
-fn main() {
-    let stdout = stdout();
-    let message = String::from("Hellow fellow Rustaceans!");
-    let width = message.chars().count();
+use std::cmp::Ordering;
+use std::io::stdin;
+use rand::Rng;
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_str(), width, &mut writer).unwrap();
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
